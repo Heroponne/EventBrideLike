da4b006031f8223829a620a8ea7207abde19d1c0
Heroponne <florence.orgeret@gmail.com>
2019-11-05 22:54:15 +0100

    modif show event for everyone and note just for authenticate user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :new]
+  before_action :authenticate_user!, only: [:edit, :new, :create]
 
   def index
   end
