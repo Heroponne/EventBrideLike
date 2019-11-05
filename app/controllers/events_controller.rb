67154f192db1b6f9f39d8446f41083df6e3e3f67
Heroponne <florence.orgeret@gmail.com>
2019-11-05 22:50:04 +0100

    event show and new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :edit, :new]
+
   def index
   end
 
   def show
+  	@event = Event.find(params[:id])
   end
-  
+
   def new
+  end
+
+  def edit
+  end
+
+  def create
+  	@event = Event.new(title: params[:title], start_date: params[:start_date], duration: params[:duration], description: params[:description], price: params[:price].to_i, location: params[:location], admin: current_user)
+
+  	if @event.save
+      flash[:success] = "Evénement bien créé !"
+      redirect_to event_path(@event.id)
+  	else
+      render :new
+      flash[:danger] = @event.errors.messages
+  	end
   end
 end
