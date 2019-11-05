77c6acab8e7f0492b9e5b21291d2aa5431f6cb59
Heroponne <florence.orgeret@gmail.com>
2019-11-05 15:09:54 +0100

    migrate modified

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_084634) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "ecrypted_password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.text "description"
     t.string "first_name"
     t.string "last_name"
